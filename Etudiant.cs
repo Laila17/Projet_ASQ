223ab02b1ae4ea2968e62978691f0c6f9c5b0aa2
Karima-Kebir <82175126+Karima-Kebir@users.noreply.github.com>
2021-04-08 21:38:41 -0400

    Add files via upload

---
+++ Etudiant.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public class Etudiant
+    {
+        public int NumeroEtudiant { get; set; }
+        public string Nom {get; set; }
+        public string Prenom { get; set; }
+        public List<Cours> listeDeCours { get; set; }
+      
+
+        //Constructeur
+        public Etudiant(int numeroEtudiant, string nom, string prenom) {
+            this.NumeroEtudiant = numeroEtudiant;
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.listeDeCours = new List<Cours>();
+        }
+
+        //Méthode ajouterCours()
+        public void ajouterCours(Cours cours) {
+            this.listeDeCours.Add(cours);
+        }
+
+        public void ajouterCoursAuListView(ListView listView) {
+            ListViewItem item;
+           
+            foreach (Cours cours in listeDeCours) {
+                item = new ListViewItem(new String[] { cours.NumeroCours.ToString(), cours.CodeCours, cours.TitreCours });
+                listView.Items.Add(item);
+            }
+        }
+
+
+    }
+}
