05264c41d0666e485f338178a38d59e8642fd2d3
Mouna Tebourski <2683364@collegelacite.ca>
2021-04-11 11:07:04 -0400

    Ajout Classe Cours

---
+++ projet_AQL/projet_AQL/Cours.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projet_AQL
+{
+    public class Cours
+    {
+        public int NumeroCours { get; set; }
+        public int Code { get; set; }
+        public string Titre { get; set; }
+
+        //Constructeur 
+        public Cours(int numero, int code, string titre)
+        {
+            this.NumeroCours = numero;
+            this.Code = code;
+            this.Titre = titre;
+        }
+
+    }
+}
