8c661212506d2484496d5b59a8c625c58e7a3b3e
Karima Kebir <82175126+Karima-Kebir@users.noreply.github.com>
2021-04-10 22:37:53 -0400

    commit initial du projet

---
+++ projet_AQL/projet_AQL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projet_AQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
